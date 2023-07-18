2fb0c4294e9fbc9dfec27c770a0f9cf429f0e135
Carsten Birck Jensen <g@bircks.dk>
2023-07-18 10:02:26 +0200

    Implement equals sign press in app

--- calculator.py
+++ calculator.py
@@ -14,14 +14,6 @@ class CalculatorLayout(Widget):
 
 
 
-    def equals(self):
-        prior = self.ids.calc_window.text
-        if '+' in prior:
-            num_list = prior.split('+')
-            answer = 0.0
-            for number in num_list:
-                answer = answer + float(number)
-            self.ids.calc_window.text = f'{answer}'
 
     def dot(self):
         if self.ids.calc_window.text[-1] in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']:
@@ -54,7 +46,6 @@ class CalculatorApp(App):
         prior = self.calculator_frontend.ids.calc_window.text
         self.calculator_frontend.ids.calc_window.text = f'{prior}{sign}'
 
-
     def clear(self):
         self.calculator_frontend.ids.calc_window.text = '0'
 
@@ -63,6 +54,16 @@ class CalculatorApp(App):
             self.calculator_frontend.ids.calc_window.text = self.calculator_frontend.ids.calc_window.text[:-1]
         else:
             self.calculator_frontend.ids.calc_window.text = '0'
+
+    def equals(self):
+        prior = self.calculator_frontend.ids.calc_window.text
+        if '+' in prior:
+            num_list = prior.split('+')
+            answer = 0.0
+            for number in num_list:
+                answer = answer + float(number)
+            self.calculator_frontend.ids.calc_window.text = f'{answer}'
+
 
     def build(self):
         return self.calculator_frontend
